f7da6b11f99318afc03d9a1bbc3a93e5a94d3a24
Tanisha-021158 <tanishasaha225@gmail.com>
2025-07-15 20:05:01 +0530

    Adding location extraction feature

--- server/server.py
+++ server/server.py
@@ -2,6 +2,10 @@ from flask import Flask,request,jsonify
 
 app=Flask(__name__)
 
+@app.route("/hello")
+def hello():
+    return "hi"
+
 if __name__=="__main__":
     print("Starting Python Flask server for Home prediction...")
     app.run()
