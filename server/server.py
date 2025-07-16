f50e0af1e0276b4c3ee86b7e2b983786e8d6e567
Tanisha-021158 <tanishasaha225@gmail.com>
2025-07-16 20:14:11 +0530

    Added backend

--- server/server.py
+++ server/server.py
@@ -1,10 +1,29 @@
 from flask import Flask,request,jsonify
+import util
+util.load_saved_artifacts()
 
 app=Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "hi"
+@app.route("/get_location_names")
+def get_location_names():
+    response=jsonify({
+        "locations":util.get_location_names()
+    })
+    response.headers.add("Access-Control-Allow-Origin","*")
+    return response
+
+@app.route("/predict_home_price",methods=['POST'])
+def predict_home_price():
+    total_sqft=float(request.form["total_sqft"])
+    bath=int(request.form["bath"])
+    bhk=int(request.form["bhk"])
+    location=request.form["location"]
+
+    response=jsonify({
+        "estimated_price":util.get_estimated_price(total_sqft,bath,bhk,location)
+    })
+
+    return response
 
 if __name__=="__main__":
     print("Starting Python Flask server for Home prediction...")
--- server/util.py
+++ server/util.py
@@ -1,9 +1,25 @@
 import json
 import pickle
+import numpy as np
+import pandas as pd
 
 __location=None
 __data_columns=None
 __model=None
+
+def get_estimated_price(sqft,bath,bhk,location):
+    try:
+        loc_idx=__data_columns.index(location.lower())
+    except:
+        loc_idx=-1
+    x=np.zeros(len(__data_columns))
+    x[0]=sqft
+    x[1]=bath
+    x[2]=bhk
+    if loc_idx>=0:
+        x[loc_idx]=1
+    return round(__model.predict([x])[0],2)
+
 
 def get_location_names():
     return __location
@@ -16,7 +32,8 @@ def load_saved_artifacts():
     with open("./artifacts/columns.json","r") as f:
         __data_columns=json.load(f)["data_columns"]
         __location=__data_columns[3:]
-
+    
+    global __model
     with open("./artifacts/banglore_home_prices_model.pickle","rb") as f:
         __model=pickle.load(f) 
     print("loading saved artifacts...done")
@@ -26,3 +43,7 @@ def load_saved_artifacts():
 if __name__=="__main__":
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price(1000,2,2,"1st Phase JP Nagar"))
+    print(get_estimated_price(1000,3,3,"1st Phase JP Nagar"))
+    print(get_estimated_price(1000,2,2,"Kalhali"))
+    print(get_estimated_price(1000,2,2,"Ejipura"))
